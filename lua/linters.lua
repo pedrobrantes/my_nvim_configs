4cbe7d30d4a7afff6b7e356872b5e39162e7b087
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-06-08 20:45:53 -0300

    chore(none-ls): update and triage linter and formatter lists
    Populate  and  with all tools
    installed via Mason.
    
    Comment out sources that were causing
    errors at startup. This organizes the files to create a stable base
    and allows for systematic re-enabling and debugging of each source.

--- lua/linters.lua
+++ lua/linters.lua
@@ -1,5 +1,31 @@
 local linters = {
-  'eslint_d'
+  'actionlint',
+  --'ansible_lint',
+  --'ast_grep',
+  --'biome',
+  'buf',
+  'cfn_lint',
+  'checkmake',
+  'clj_kondo',
+  --'cmakelint',
+  'djlint',
+  'dotenv_linter',
+  --'eslint_d',
+  --'hlint',
+  --'kube_linter',
+  --'kulala_fmt',
+  'npm_groovy_lint',
+  'rstcheck',
+  'semgrep',
+  --'shellharden',
+  'stylelint',
+  'swiftlint',
+  --'systemdlint',
+  --'tflint',
+  'trivy',
+  'vacuum',
+  'vale',
+  'yamllint'
 }
 
 return linters
