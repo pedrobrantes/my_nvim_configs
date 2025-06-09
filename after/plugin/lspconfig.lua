573b75e77ec7b6cef2a4863d84e1cae21a8ca729
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-06-08 21:11:54 -0300

    fix(which-key): remove conflicting registration from lspconfig
    Remove the `which-key` registration (`wk.register`) from the `LspAttach`
    autocommand in `lspconfig.lua`.
    
    This conflicting, old-spec registration was causing startup errors
    and a problematic `:checkhealth` report for `which-key`. LSP-related
    keymaps are now managed centrally in `mappings.lua`.

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -30,25 +30,8 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
-    local wk = require('which-key')
     -- Enable completion triggered by <c-x><c-o>
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    wk.register({
-      l = {
-        name = 'LSP',
-        d = { '<cmd>lua vim.lsp.buf.declaration()<cr>', 'Declaration' },
-        D = { '<cmd>lua vim.lsp.buf.definition()<cr>', 'Definition' },
-        h = { '<cmd>lua vim.lsp.buf.hover()<cr>', 'Hover' },
-        i = { '<cmd>lua vim.lsp.buf.implementation()<cr>', 'Implementation' },
-        s = { '<cmd>lua vim.lsp.buf.signature_help()<cr>', 'Signature Help' },
-        r = { '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename' },
-        t = { '<cmd>lua vim.lsp.buf.type_definition()<cr>', 'Type Definition' },
-        c = { '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Code action' },
-        f = { '<cmd>lua vim.lsp.buf.format()<cr>', 'Format file' },
-        e = { '<cmd>lua vim.diagnostic.open_float()<cr>', 'Show diagnostic' },
-      }
-    }, { prefix = '<space>' })
   end,
 })
 
@@ -58,9 +41,3 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
--- vim.diagnostic.config({
---  virtual_text = {
---    prefix = '' 
---  }
--- })
