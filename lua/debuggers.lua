c0d1ee988e04dc7784f9178dfa04252c90a8a584
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-06-08 21:07:13 -0300

    feat(dap): modularize list of debug adapters
    Create the `lua/debuggers.lua` file to hold the list of DAP packages
    that should be installed and configured by `mason-nvim-dap.nvim`.
    
    This keeps the main `plugin_manager.lua` file cleaner and makes it
    easier to manage DAP installations.

---
+++ lua/debuggers.lua
@@ -0,0 +1,21 @@
+local dap_packages = {
+  'bash-debug-adapter',
+  'chrome-debug-adapter',
+  'cpptools',
+  'dart-debug-adapter',
+  'debugpy',
+  'delve',
+  'elixir-ls',
+  'haskell-debug-adapter',
+  'java-debug-adapter',
+  'js-debug-adapter',
+  'kotlin-debug-adapter',
+  'local-lua-debugger-vscode',
+  'netcoredbg',
+  'node-debug2-adapter',
+  'ocamlearlybird',
+  'perl-debug-adapter',
+  'php-debug-adapter'
+}
+
+return dap_packages
